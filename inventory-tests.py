dae79a31884b609409ee3ab00aa856671427eba2
dansman805 <dansman805@gmail.com>
2017-09-06 21:43:29 -0400

    Fixed writeInvStruvture, writeInv, adnd added Player class

--- inventory-tests.py
+++ inventory-tests.py
@@ -5,16 +5,22 @@ from main import *
 testInventoryPath = '.\\testinventory.txt'
 
 class GameTest(unittest.TestCase):
-    def test_inv_writing(self):
-        os.remove(testInventoryPath)
-
-        with open(inventoryPath,'a+') as file:
-            writeInvStructure(file) #Creates inventory file and creates inventory structure
+    def test_inv_creation(self):
+        resetInventory(testInventoryPath)
 
         with open(inventoryPath, 'r') as file:
             currentTestInventory = file.read()
+        self.assertEqual(invStructure, currentTestInventory, 'Inventory Structure Writing')
 
-        self.assertEqual(invStructure, currentTestInventory, 'Inventory Structure Writing Functional')
+    def test_inv_writing_reading(self):
+        resetInventory(testInventoryPath)
+
+        slot = 0
+        for slot in xrange(10):
+            writeInv(slot, 'item', testInventoryPath)
+        self.assertEqual(['item']*10, readInv(testInventoryPath), 'Inventory Read and Writing')
+
+resetInventory(testInventoryPath)
 
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import os
 
+class Player:
+    #A class for a player
+    def __init__(self):
+        pass
+
+
 inventoryPath = '.\\inventory.txt'
 invStructure = 'Slot0:\nSlot1:\nSlot2:\nSlot3:\nSlot4:\nSlot5:\nSlot6:\nSlot7:\nSlot8:\nSlot9:'
+
+def resetInventory(f):
+    try:
+        os.remove(f)
+    except WindowsError:
+        pass
+
+    with open(f,'a+') as file:
+        writeInvStructure(file) #Creates inventory file and creates inventory structure
 
 def readInv(file): #reads what is contained in inventory
     with open(file, 'r') as f:
         lines = f.readlines()
-        for line in range(0, len(lines)):
-            lines[line] = lines[line][5:]
+        for line in range(len(lines)):
+            lines[line] = lines[line][6:-1]
     return lines
 
 def writeInv(slot, item, file):
     with open(file, 'r') as f:
         content = f.readlines()
-    content[slot] = item + '\n'
+    content += ['Slot' + str(slot) + ':' + item + '\n']
 
     with open(file, 'w') as f:
         f.writelines(content)
+
 def writeInvStructure(file):
     with open(inventoryPath, 'w') as text_file:
         text_file.write(invStructure)
